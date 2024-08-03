38291b524d6f7e6f2f3fa2feb4140b22f87d3924
Kubbo <390378816@qq.com>
2024-08-03 14:30:17 +0800

    增加loading消息

--- dowmd.py
+++ dowmd.py
@@ -8,7 +8,7 @@ import plugins
 from bridge.reply import Reply, ReplyType
 from common.log import logger
 from plugins import *
-
+from lib import itchat
 
 @plugins.register(
     name="dow_markdown",
@@ -21,16 +21,26 @@ class dow_markdown(Plugin):
     def __init__(self):
         super().__init__()
         try:
+            self.handlers[Event.ON_HANDLE_CONTEXT] = self.on_handle_context
             self.handlers[Event.ON_DECORATE_REPLY] = self.on_decorate_reply
             logger.info("[dow_markdown] inited.")
         except Exception as e:
             logger.warn("[dow_markdown] init failed, ignore.")
             raise e
+    def on_handle_context(self,e_content:EventContext):
+        try:
+            send_msg = e_context["context"]
+            if send_msg["type"] == ReplyType.TEXT:
+                if any(word in send_msg["content"] for word in ["画"]):
+                    receiver = send_msg.get("receiver")
+                    itchat.send("我正在绘画中,可能需要多等待一会,请稍后...",toUserName=receiver)
+                    e_content.action = EventAction.CONTINUE
+        finally:
+            e_content.action = EventAction.CONTINUE
     def on_decorate_reply(self, e_context: EventContext):
         if e_context["reply"].type != ReplyType.TEXT:
             return
         try:
-            logger.info(f"获取到回复消息{e_context}")
             channel = e_context["channel"]
             context = e_context["context"]
             content = e_context["reply"].content.strip()
@@ -38,10 +48,9 @@ class dow_markdown(Plugin):
             if content.startswith("[DOWNLOAD_ERROR]"):
                 return
             has_md = re.search(r'\!\[[^\]]+\]\(?',content)
-            logger.info(f"{content}是否含有md语法==>{has_md}")
             if has_md:
                 host = os.environ.get('DIFY_API_BASE', 'http://121.37.155.68:35801')
-                image_path = re.search(r'!\[.*\]\((.*?)\)',reply.content).group(1)
+                image_path = re.search(r'!\[.*\]\((.*?)\)',content).group(1)
                 logger.info(f"提取到的数据==>host:{host},url:{image_path}")
                 reply = Reply(ReplyType.IMAGE_URL, f"{host}{image_path}")
                 e_context["reply"] = reply
